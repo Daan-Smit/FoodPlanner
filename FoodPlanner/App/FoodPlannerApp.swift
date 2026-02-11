56a9789c69a9c0cb2ae85c812153254c30b0fe5a
Daan Smit <daan.smit@kpnmail.nl>
2026-02-11 10:57:02 +0100

    deleted files

---
+++ FoodPlanner/App/FoodPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPlannerApp.swift
+//  FoodPlanner
+//
+//  Created by DaanSmit on 11/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct FoodPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
